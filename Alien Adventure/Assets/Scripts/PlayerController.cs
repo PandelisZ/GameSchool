30e444e1f01abbe2e3f5b990438d747fdb103566
Pandelis Zembashis <info@pandelis.me>
2014-09-10 07:48:32 +0100

    Uploading Alien Adventure game with basic physics
    Basic physics and controls implemented with ray casting for collision
    detection

---
+++ Alien Adventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+public class PlayerController : MonoBehaviour {
+
+	public float gravity = .1f;
+	public float speed = .1f;
+	public float acceleration = .3f;
+
+	private float currentSpeed;
+	private float targetSpeed;
+	private Vector2 amountToMove;
+
+	private PlayerPhysics playerPhysics;
+
+	// Use this for initialization
+	void Start () {
+		playerPhysics = GetComponent<PlayerPhysics>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetSpeed = Input.GetAxisRaw ("Horizontal") * speed;
+		currentSpeed = IncrementTowards (currentSpeed, targetSpeed, acceleration);
+
+		amountToMove.x = currentSpeed;
+		amountToMove.y -= gravity * Time.deltaTime;
+		playerPhysics.Move(amountToMove);
+	}
+
+	private float IncrementTowards(float n, float target, float a){
+		if (n == target) {
+						return n;
+				} else {
+			float dir = Mathf.Sign (target - n);
+			n +=a * Time.deltaTime * dir;
+			       return (dir == Mathf.Sign(target-n))? n: target;
+				}
+	}
+}
---
+++ Alien Adventure/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider))]
+public class PlayerPhysics : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	private BoxCollider collider;
+	private Vector3 s;
+	private Vector3 c;
+
+	private float skin = .005f;
+
+	[HideInInspector]
+	public bool grounded;
+
+	Ray ray;
+	RaycastHit hit;
+
+	void Start () {
+		collider = GetComponent<BoxCollider>();
+		s = collider.size;
+		c = collider.center;
+	}
+
+	public void Move (Vector2 moveAmount){
+
+		float deltaY = moveAmount.y;
+		float deltaX = moveAmount.x;
+		Vector2 p = transform.position;
+
+		for (int i = 0; i < 3; i++) {
+			float dir = Mathf.Sign(deltaY);
+			float x = (p.x + c.x -s.x/2) + s.x/2 * i;
+			float y = p.y + c.y + s.y/2 * dir;
+
+			ray = new Ray(new Vector2(x,y), new Vector2(0,dir));
+
+			if (Physics.Raycast (ray,out hit,Mathf.Abs(deltaY), collisionMask)){
+				float dst = Vector3.Distance (ray.origin, hit.point);
+
+				if (dst > skin){
+					deltaY = -dst + skin;
+				}
+				else {
+					deltaY = 0;
+				}
+				grounded = true;
+				break;
+			}
+		}
+
+		Vector2 finalTransform = new Vector2(deltaX, deltaY);
+
+		transform.Translate(finalTransform);
+	}
+
+}
